9fc498d9353e95b2b2ef65cc6847a86ebd6ef525
David <David@DAVID-PC>
2017-02-23 22:29:07 +1000

    Add a example of reading files from cloud storage.

--- WebApplication2/Controllers/HomeController.cs
+++ WebApplication2/Controllers/HomeController.cs
@@ -4,6 +4,11 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using Microsoft.Azure; // Namespace for Azure Configuration Manager
+using Microsoft.WindowsAzure.Storage; // Namespace for Storage Client Library
+using Microsoft.WindowsAzure.Storage.Blob; // Namespace for Blob storage
+using Microsoft.WindowsAzure.Storage.File; // Namespace for File storage
+
 namespace WebApplication2.Controllers
 {
     public class HomeController : Controller
@@ -15,7 +20,51 @@ namespace WebApplication2.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            // Parse the connection string and return a reference to the storage account.
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+                CloudConfigurationManager.GetSetting("StorageConnectionString"));
+
+            // Create a CloudFileClient object for credentialed access to File storage.
+            CloudFileClient fileClient = storageAccount.CreateCloudFileClient();
+
+            // Get a reference to the file share we created previously.
+            CloudFileShare share = fileClient.GetShareReference("backupsystem");
+
+            // Ensure that the share exists.
+            if (share.Exists())
+            {
+                // Get a reference to the root directory for the share.
+                CloudFileDirectory rootDir = share.GetRootDirectoryReference();
+
+
+                /* Example of getting a list of files */
+                /*string sResult = "";                
+                foreach (IListFileItem fileItem in rootDir.ListFilesAndDirectories())
+                {
+                    sResult += fileItem.Uri.LocalPath;                
+                }*/
+
+                // Get a reference to the directory we created previously.
+                CloudFileDirectory sampleDir = rootDir.GetDirectoryReference("TestDirectory");
+
+                // Ensure that the directory exists.
+                if (sampleDir.Exists())
+                {
+                    
+                    // Get a reference to the file we created previously.
+                    CloudFile file = sampleDir.GetFileReference("TestEnglish.txt"); 
+
+                    // Ensure that the file exists.
+                    if (file.Exists())
+                    {
+                        // Write the contents of the file to the console window.
+                        //Console.WriteLine(file.DownloadTextAsync().Result);
+                        ViewBag.Message = file.DownloadTextAsync().Result;
+                    }
+                }
+            }
+
+            //ViewBag.Message = "Your application description page.";
 
             return View();
         }
